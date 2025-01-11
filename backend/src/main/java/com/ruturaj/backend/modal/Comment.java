e76a6ba69176a476e89b86f302e2318d464f2b17
“[github]” <ruturajjadhav122@gmail.com>
2025-01-11 15:01:20 +0530

    initial commit

---
+++ backend/src/main/java/com/ruturaj/backend/modal/Comment.java
@@ -0,0 +1,36 @@
+package com.ruturaj.backend.modal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="content")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="post_id")
+    private Post post;
+
+}
